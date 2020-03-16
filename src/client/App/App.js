2c53fee21d191950318b3cceb81f75c7c6683c78
James 'Mac' Read <james.macgregor.read@gmail.com>
2020-03-16 10:06:47 -0600

    Installed express server/rearranged folders

--- src/client/App/App.js
+++ src/client/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import SignIn from './components/SignIn';
+import SignIn from '../components/SignIn';
 import 'typeface-roboto';
 
 function App() {
